ecb11a2df74abc55b152d7fe581cc5744ee63e04
Vishnu Subramanian <vishnu.subramanian93@gmail.com>
2026-02-23 19:37:32 +0530

    Phase 1 | Step 3/10 | tools/servicenow_tools.py defined

---
+++ tools/servicenow_tools.py
@@ -0,0 +1,24 @@
+def extract_request_details(state):
+    
+    try:
+        
+        raw = state["raw_input"];
+
+        normalized = \
+        {
+            "request_number": raw["request_number"],
+            "ritm_number": raw["ritm_number"],
+            "email": raw["email"],
+            "role": raw["role"],
+            "environment": raw["environment"],
+            "country": raw["country"]
+        };
+
+        state["normalized"] = normalized;
+        return state;
+
+    except Exception as e:
+        state["errors"] = str(e);
+        return state;
+
+
