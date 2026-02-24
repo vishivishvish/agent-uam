16a5652c16d565a9d3d132a8778b1e445cc438e4
Vishnu Subramanian <vishnu.subramanian93@gmail.com>
2026-02-24 19:43:35 +0530

    Phase 2 | Step 5/10 | tools/registry.py modified

--- tools/registry.py
+++ tools/registry.py
@@ -1,10 +1,16 @@
-from tools.servicenow_tools import extract_request_details;
+from tools.servicenow_tools import validate_and_normalize_request;
 from tools.mapping_tools import resolve_approver;
+from tools.approval_tools import simulate_approval;
+from tools.veeva_tools import check_user_exists, create_user, close_request;
 
 TOOL_REGISTRY = \
 {
-    "extract_request_details": extract_request_details,
-    "resolve_approver": resolve_approver
-}
+    "validate_and_normalize_request": validate_and_normalize_request,
+    "resolve_approver": resolve_approver,
+    "simulate_approval": simulate_approval,
+    "check_user_exists": check_user_exists,
+    "create_user": create_user,
+    "close_request": close_request  
+};
 
 print("All good - tools/registry.py now ready to be used");
