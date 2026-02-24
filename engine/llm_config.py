07d818ab0d9c822ac2381a0206eb5e8a0d5c509f
Vishnu Subramanian <vishnu.subramanian93@gmail.com>
2026-02-24 15:51:45 +0530

    Phase 2 | Step 2/10 | engine/llm_config.py defined

---
+++ engine/llm_config.py
@@ -0,0 +1,16 @@
+from langchain_openai import AzureChatOpenAI;
+
+azure_key = "e6cd8b069c0243bb877b9d202fac9f74";
+azure_endpoint = "https://llmcoesre-openai.openai.azure.com/";
+azure_model = "gpt-5.1";
+api_version = "2025-04-01-preview";
+temperature = 0;
+
+llm = AzureChatOpenAI\
+(
+    api_key = azure_key,
+    azure_endpoint = azure_endpoint,
+    deployment_name = azure_model,
+    api_version = api_version,
+    temperature = temperature
+);
