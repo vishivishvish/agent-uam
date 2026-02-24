b5cf6811636e55cb44aaef53609cfb9715d987d9
Vishnu Subramanian <vishnu.subramanian93@gmail.com>
2026-02-24 19:39:37 +0530

    Phase 2 | Step 4/10 | tools/veeva_tools.py defined

--- tools/veeva_tools.py
+++ tools/veeva_tools.py
@@ -4,3 +4,29 @@ def check_user_exists(email):
     email = state["normalized"]["email"];
     state["user_exists"] = email in EXISTING_USERS;
     return state;
+
+def create_user(state):
+
+    if state["user_exists"]:
+        state["provisioning_result"] = \
+        {
+            "message": "User already exists"
+        };
+        return state;
+
+    state["provisioning_result"] = \
+    {
+        "username": "generated_user_123",
+        "status": "ACTIVE",
+        "system" : "Veeva Vault"
+    };
+
+    return state;
+
+def close_request(state):
+
+    if not state.get("provisioning_result"):
+        return state;
+
+    state["provisioning_result"]["servicenow_status"] = "Closed Complete";
+    return state;
