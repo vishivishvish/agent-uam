b3f4cf1c8c5d46c62a4df89bd5bc1558ecb76df0
Vishnu Subramanian <vishnu.subramanian93@gmail.com>
2026-02-25 14:06:31 +0530

    Fixed an error in tools/veeva_tools.py

--- tools/veeva_tools.py
+++ tools/veeva_tools.py
@@ -1,6 +1,6 @@
 EXISTING_USERS = ["john.doe@company.com"];
 
-def check_user_exists(email):
+def check_user_exists(state):
     email = state["normalized"]["email"];
     state["user_exists"] = email in EXISTING_USERS;
     return state;
